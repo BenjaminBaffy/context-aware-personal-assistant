39aab3b6328c516ce3fcea9098cdb8197fd6717f
Adam Kara <kara.p.adam@gmail.com>
2021-10-21 16:38:18 +0200

    Correct demo api service

--- context-aware-personal-assistant-frontend/src/services/api/DemoService.ts
+++ context-aware-personal-assistant-frontend/src/services/api/DemoService.ts
@@ -2,7 +2,7 @@ import ApiService from "./ApiService"
 
 const DemoService = {
     getDemoStuff: (arg: number) => ApiService.get(`Demo/${arg}`),
-    postDemoStuff: (arg: number, data: Object) => ApiService.get(`Demo/${arg}`, data),
+    postDemoStuff: (arg: number, data: Object) => ApiService.post(`Demo/${arg}`, data),
 }
 
 export default DemoService
