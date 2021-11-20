f6cfa1ebdd02d11684d887fb0eb3ebfd262b3250
Elshan <elshanqadimov7@gmail.com>
2021-11-20 17:55:16 +0100

    city extractor and name extractor work fine together

--- backend/assistant_core/actions/actions.py
+++ backend/assistant_core/actions/actions.py
@@ -5,7 +5,6 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-# from rasa_sdk.events import SlotSet
 from src.wx import wx_city
 
 
@@ -20,91 +19,17 @@ class Weather(Action):
 
         # get the current slot values from rasa
         city = tracker.get_slot('city')
-        # wx_type = tracker.get_slot('wx_type')
-        # forecast_period = tracker.get_slot('forecast_period')
-
-        # set values for empty slots
-        # if forecast_period is None:
-        #     forecast_period  = "current"
-        # if wx_type is None:
-        #     wx_type = "weather"
-
-        # set the forecast steps for OpenWeather forecast json format 
-        # drow = 0
-        # if forecast_period == "tomorrow":
-        #    drow = 1
-        
-        # list possible cities     
-        # possible_cities=["Budapest","Debrecen","Szeged"]
 
         # Default answer if better answer cannot be found
-        # response="Sorry, got no idea - but I hope it's going to be sunny and warm. "
-
-        # read & parse the information and generate response
-        if True:
+        response="Sorry, got no idea - but I hope it's going to be sunny and warm. "
             
-            # get the weather information from function defined in wx.py
-            open_wx_msg = wx_city(city)
+        # get the weather information from function defined in wx.py
+        open_wx_msg = wx_city(city)
 
-            # set values to current weather variables from open_wx_msg json
-            temp=round(open_wx_msg['main']['temp'])
-            # pressure=round(open_wx_msg['main']['pressure'])
-            # humidity=round(open_wx_msg['main']['humidity'])
-            description=(open_wx_msg['weather'][0]["description"])
+        # set values to current weather variables from open_wx_msg json
+        temp=round(open_wx_msg['main']['temp'])
+        description=(open_wx_msg['weather'][0]["description"])
 
-            response = "The current temperature in {} is {}C. It is {}".format(city, temp, description)
-
-            # # set predicted rainfall to 0 if field missing - OpenWeather API seems to not return it at all if 0
-            # try: 
-            #     rain_predict=round(open_wx_msg['daily'][drow]['rain'],1)
-            # except:
-            #     rain_predict=0
-
-
-            # answers to queries about current weather    
-            # if True: 
-            #     # generic query
-            #     if wx_type=='weather': 
-            #         response = "The current temperature in {} is {}C. It is {} and the wind speed is {}m/s".format(city, temp, cond, wind)
-            #     # specific information
-            #     if wx_type=='wind':
-            #         response = "The current wind speed in {} is {} metres per second from {} degrees. ".format(city, wind, wind_deg)
-            #     if wx_type=='temperature':
-            #         response = "The current temperature in {} is {}degrees Celsius. ".format(city,temp)
-            #     if wx_type=='pressure':
-            #         response="The current air pressure in {} is {} millibars. ".format(city,pressure)
-            #     if wx_type=='humid':
-            #         response="The current humidity in {} is {}%. ".format(city,humidity)
-            #     if wx_type=='uvi':
-            #         response="The current UV index in {} is {}. ".format(city,uvi)
-            #     if wx_type=='cloud_conditions':
-            #         response="The current conditions in {} is {}.".format(city,cond)
-
-
-            # answers to forecasts for today and tomorrow   
-            # if forecast_period == 'today' or forecast_period == 'tomorrow':
-            #     # generic forecast
-            #     if wx_type=='weather':
-            #         response = "The forecast high for {} {} is {}C. It is expected to be {} and the wind speed is {}m/s".format(city, forecast_period, temp_max_predict, cond_predict, wind_speed_predict)
-            #     # more specific forecasts
-            #     if wx_type=='wind':
-            #         response = "The forecasted wind speed for {} {} is {} metres per second from {} degrees. ".format(city, forecast_period, wind_speed_predict, wind_deg_predict)
-            #     if wx_type=='temperature':
-            #         response = "The forecasted maximum temperature for {} {} is {}C while the minimum is {}C. ".format(city, forecast_period,temp_max_predict, temp_min_predict)
-            #     if wx_type=='pressure':
-            #         response="The forecasted air pressure for {} {} is {} millibars. ".format(city, forecast_period,pressure_predict)
-            #     if wx_type=='humid':
-            #         response="The forecasted humidity for {} {} is {}%. ".format(city, forecast_period,humidity_predict)
-            #     if wx_type=='uvi':
-            #         response="The forecasted UV index for {} {} is {}. ".format(city, forecast_period,uvi_predict)
-            #     if wx_type=='cloud_conditions':
-            #         response="The predicted conditions for {} {} is {} and 24hrs rain fall is expected to be {}mm".format(city, forecast_period,cond_predict, rain_predict)
-                
-        # reponse for if city is not in range of supported cities
-        else:
-            response = "Sorry, currently I am limited to information from Budapest, Debrecen and Szeged only."
-
+        response = "The current temperature in {} is {}C. It is {}".format(city, temp, description)
 
         return dispatcher.utter_message(response)
-
-        # return []
--- backend/assistant_core/src/yml lookup tools/txt_to_yml.py
+++ backend/assistant_core/src/yml lookup tools/txt_to_yml.py
@@ -2,26 +2,26 @@
 ############################# yml creator for names ########################################
 ############################################################################################
 
-# # Insert normal .txt/.csv path here
-# file = open("names.txt", 'r')
+# Insert normal .txt/.csv path here
+file = open("names.txt", 'r')
 
-# # Splits each row
-# lines = file.read().splitlines()
-# n = 0
+# Splits each row
+lines = file.read().splitlines()
+n = 0
 
-# # Insert target output path here
-# file2 = open("../data/names.yml", "w")
+# Insert target output path here
+file2 = open("../data/names.yml", "w")
 
-# # Writes the headers(?), remember to change the lookup name
-# file2.write("version: \"2.0\"\nnlu:\n  - lookup: name  \n    examples: |\n")
+# Writes the headers(?), remember to change the lookup name
+file2.write("version: \"2.0\"\nnlu:\n  - lookup: name  \n    examples: |\n")
 
-# # Adds indent and dash to each line
-# for line in lines:
-#     file2.write("      - " + str(line) + "\n")
+# Adds indent and dash to each line
+for line in lines:
+    file2.write("      - " + str(line) + "\n")
 
-# # Closes the files
-# file.close()
-# file2.close()
+# Closes the files
+file.close()
+file2.close()
 
 ############################################################################################
 ################################ yml creator for cities ####################################
