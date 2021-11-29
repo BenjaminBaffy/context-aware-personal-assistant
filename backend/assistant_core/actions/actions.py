5980d363d92c935131e241f6f62e32211d16df53
Elshan <39336620+Elshan777@users.noreply.github.com>
2021-11-29 10:36:15 +0100

    Feature/grocery (#46)
    * weather without asking the city works
    
    * weather without asking the city works 2
    
    * grocery intent classification bug fixed
    
    * unnecessary models removed
    
    * it is possible to add and display items in grocery list,  lookup table is working as well for the given feature
    
    * grocery feature is finished

--- backend/assistant_core/actions/actions.py
+++ backend/assistant_core/actions/actions.py
@@ -7,6 +7,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from src.wx import wx_city
 
+GROCERY_LIST = []
 
 class Weather(Action):
 
@@ -31,5 +32,69 @@ class Weather(Action):
         description=(open_wx_msg['weather'][0]["description"])
 
         response = "The current temperature in {} is {}C. It is {}".format(city, temp, description)
+
+        return dispatcher.utter_message(response)
+
+class GroceryAddDisplay(Action):
+
+    def name(self) -> Text:
+        return "action_add_gr"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        groceries = tracker.get_slot('items')
+        # if groceries:
+        GROCERY_LIST.extend(groceries)
+    
+        groceries_str = '\n'.join(GROCERY_LIST)
+        response = f"Items in your grocery list: \n{groceries_str}"
+        
+        # else: 
+        #     response = "Currently your grocery list is empty. \nAdd something"
+
+        return dispatcher.utter_message(response)
+
+
+class GroceryDisplay(Action):
+
+    def name(self) -> Text:
+        return "action_ls_gr"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        if GROCERY_LIST:      
+            groceries_str = '\n'.join(GROCERY_LIST)
+            response = f"Items in your grocery list: \n{groceries_str}"
+        else: 
+            response = "Currently your grocery list is empty. \nAdd something..."
+
+        return dispatcher.utter_message(response)
+
+
+class GroceryRemove(Action):
+
+    def name(self) -> Text:
+        return "action_rm_gr"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        groceries = tracker.get_slot('items')
+
+        for element in GROCERY_LIST:
+            if element in groceries:
+                GROCERY_LIST.remove(element)
+
+        if GROCERY_LIST:
+            groceries_str = '\n'.join(GROCERY_LIST)
+            response = f"Items in your grocery list: \n{groceries_str}"
+        
+        else: 
+            response = "Currently your grocery list is empty. \nAdd something"
 
         return dispatcher.utter_message(response)
